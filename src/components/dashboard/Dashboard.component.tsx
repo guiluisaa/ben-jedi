c5b20dfc16db5bc271da256b98941eee3ef77bee
Guilherme Araujo <guiluis.aa@gmail.com>
2020-03-03 09:59:56 -0300

    refactor: remove dashboard  container

--- src/components/dashboard/Dashboard.component.tsx
+++ src/components/dashboard/Dashboard.component.tsx
@@ -1,18 +1,43 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { Typography, Spin } from 'antd';
 import { useTranslation } from 'react-i18next';
-import { ChartData, HorizontalBar } from 'react-chartjs-2';
+import { HorizontalBar } from 'react-chartjs-2';
 
-type DashboardComponentProps = {
-  filmsData: ChartData<Chart.ChartData>;
-  isLoading: boolean;
+import useFilms from '@/io/redux/film/useFilms';
+import { Film } from '@/io/redux/film/film.types';
+
+const filmsToData = (films: Film[]) => {
+  const labels = films.map(film => film.title);
+  const data = films.map(film => film.characters.length);
+
+  const datasets = [
+    {
+      label: '# of characters',
+      data,
+      backgroundColor: [
+        '#ffd052',
+        '#27395a',
+        '#4f9a8e',
+        '#3090ad',
+        '#b92750',
+        '#f7941d',
+        '#7d889c'
+      ]
+    }
+  ];
+
+  return { labels, datasets };
 };
 
-const DashboardComponent: FC<DashboardComponentProps> = ({
-  filmsData,
-  isLoading
-}) => {
+const Dashboard: FC = () => {
   const [t] = useTranslation();
+
+  const { getFilms, films, isLoading } = useFilms();
+  const filmsData = filmsToData(films);
+
+  useEffect(() => {
+    getFilms(1);
+  }, []);
 
   return (
     <>
@@ -35,4 +60,4 @@ const DashboardComponent: FC<DashboardComponentProps> = ({
   );
 };
 
-export default DashboardComponent;
+export default Dashboard;
